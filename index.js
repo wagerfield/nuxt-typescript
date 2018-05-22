7a2acd2c0960d027504d13f692805b9d53ee274c
Matthew Wagerfield <matthew@wagerfield.com>
2018-05-22 19:28:46 +0100

    added module code

---
+++ index.js
@@ -0,0 +1,30 @@
+function extendBuild(config) {
+  const tsLoader = {
+    loader: 'ts-loader',
+    options: {
+      appendTsSuffixTo: [ /\.vue$/ ]
+    }
+  }
+
+  // Add TypeScript loader to Webpack rules
+  config.module.rules.push(Object.assign({
+    test: /((client|server)\.js)|(\.tsx?)$/
+  }, tsLoader))
+
+  // Add TypeScript loader for vue files
+  for (const rule of config.module.rules) {
+    if (rule.loader === 'vue-loader') {
+      rule.options.loaders.ts = tsLoader
+    }
+  }
+
+  // Add .ts extension in webpack resolve
+  if (config.resolve.extensions.indexOf('.ts') === -1) {
+    config.resolve.extensions.push('.ts')
+  }
+}
+
+module.exports = function nuxtTypeScript() {
+  this.nuxt.options.extensions.push('ts')
+  this.extendBuild(extendBuild)
+}
