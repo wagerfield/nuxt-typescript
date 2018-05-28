9844589528e509e930e0a2ded6c5b5c924ddfa67
Matthew Wagerfield <matthew@wagerfield.com>
2018-05-28 16:01:27 +0100

    added pure TypeScript component

--- example/pages/index.vue
+++ example/pages/index.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
     <h1 v-text="title"/>
+    <v-ts-file/>
     <v-tsx-file/>
-    <v-tsx-lang/>
     <v-ts-lang/>
+    <v-tsx-lang/>
     <v-reverse/>
   </div>
 </template>
@@ -12,6 +13,7 @@
 import { Component, Vue } from "nuxt-property-decorator"
 import { State } from "vuex-class"
 import vReverse from "../components/reverse.vue"
+import vTsFile from "../components/ts-file"
 import vTsLang from "../components/ts-lang.vue"
 import vTsxFile from "../components/tsx-file"
 import vTsxLang from "../components/tsx-lang.vue"
@@ -19,13 +21,14 @@ import vTsxLang from "../components/tsx-lang.vue"
 @Component({
   components: {
     vReverse,
+    vTsFile,
     vTsLang,
     vTsxFile,
     vTsxLang
   }
 })
 export default class extends Vue {
-  @State public title: string
+  @State public title!: string
 
   public head() {
     return {
