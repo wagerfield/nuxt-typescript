c9a35a8995cb657be3dc66846eea26b38f123d90
Matthew Wagerfield <matthew@wagerfield.com>
2018-05-28 16:14:32 +0100

    created about page using lang="tsx"

---
+++ example/pages/about.vue
@@ -0,0 +1,9 @@
+<script lang="tsx">
+import Vue, { CreateElement, VNode } from "vue"
+
+export default Vue.extend({
+  render(h: CreateElement): VNode {
+    return <h1>About</h1>
+  }
+})
+</script>
